be95471654a0e0d06628f46f84d26fcb12e53a35
jesus2102 <jesusperezmarin@gmail.com>
2018-06-08 10:43:31 +0100

    initial commit. SQLruner + cinema.sql

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE customers;
+DROP TABLE films;
+DROP TABLE tickets;
+
+CREATE TABLE customers(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+)
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  price INT
+)
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id SERIAL4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id SERIAL4 REFERENCES films(id) ON DELETE CASCADE,
+)
